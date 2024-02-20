1efaa2ef29d02dbf0c34f1d8801777aeb39a7189
Nasim Rana Feroz <feroznasimrana@gmail.com>
2024-02-20 15:45:03 +0800

    first commit

---
+++ Basics/Veriables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a[] = "Nasim";
+    printf("My name is %s.\n" , a);
+
+    int b = 23;
+    printf("My age is %d.\n" , b);
+
+    float c = 5.6;
+    double d = 7.8965;
+    printf("You get: %f, %lf\n", c, d);
+
+    return 0;
+}
